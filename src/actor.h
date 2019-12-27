77704588377f77206d6a3203e17fb73d6b190d10
David Summers <david@summersoft.fay-ar.us>
2019-12-27 14:33:04 -0600

    The Actor pattern allows multi-threaded functionality and makes it simple for a class to add a thread of execution and still be sure that its state instance variables are only accessed via that thread, disallowing variable sharing which is the bane of multi-threaded programs.  Add Actor and BlockingQueue classes in preparation for simplifying multi-threaded servers.  T

---
+++ src/actor.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <functional>
+#include <list>
+#include <string>
+
+#include "blocking_queue.h"
+
+class Actor
+{
+  public:
+
+    typedef std::function< void( ) > Message;
+
+    Actor( const std::string &name );
+    ~Actor( );
+
+    const std::string &GetName( );
+    void SetName( const std::string & );
+
+    void Send( Message );
+
+    static void SetSingleThreaded( bool value = true );
+
+  protected:
+
+  private:
+
+    //
+    // Declarations
+    //
+
+
+    //
+    // Methods
+    //
+
+    Actor( const Actor & );           // No copying.
+    void operator =( const Actor & ); // No copying.
+
+    static void Instance( );
+    static void Destroy( );
+
+    void Run( );
+
+    void ProcessAllMessageQueues( );
+    void ProcessEntireMessageQueue( );
+    void ProcessOneMessage( );
+
+    void ProcessMessage( Message & );
+
+    //
+    // Data
+    //
+
+    // Statics
+    static bool                    s_SingleThreaded;
+
+    typedef std::list< Actor * >   ActorList;
+    static ActorList              *s_ActorList;
+
+    // Non-statics
+    std::string                    m_Name;
+    bool                           m_Done;
+
+    typedef enum STATE
+    {
+      WAITING,
+      PROCESSING
+    } STATE;
+
+    STATE                          m_State;
+
+    BlockingQueue< Message >       m_MsgQueue;
+    std::unique_ptr< std::thread > m_Thread;
+};
