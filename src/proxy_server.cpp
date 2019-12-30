9e84b2098f5627a50b25d311627c91244985a3c4
David Summers <david@summersoft.fay-ar.us>
2019-12-29 18:08:13 -0600

    Add skeleton for ProxyServer, but not used yet.

---
+++ src/proxy_server.cpp
@@ -0,0 +1,91 @@
+#include "addr_info.h"
+#include "network.h"
+#include "proxy_server.h"
+#include "search_server.h"
+
+
+ProxyServer::ProxyServer( ServerList &serverList_, PacketServer &server_, Network &network_ )
+  : m_ServerList( serverList_ )
+  , m_PacketServer( server_ )
+  , m_Network( network_ )
+{
+  m_PacketServer.Register( this );
+}
+
+
+ProxyServer::~ProxyServer( )
+{
+  m_PacketServer.Unregister( this );
+}
+
+
+void ProxyServer::OnReceivePacket( sockaddr_storage client_, std::vector< uint8_t > data_ )
+{
+#ifdef LOGDATA
+  std::cout << "ProxyServer: Received packet." << std::endl;
+#endif
+
+  if ( !valid_packet( data_ ) )
+  {
+    std::cout << "ProxyServer: Received invalid frame, skipping" << std::endl;
+    return;
+  }
+
+#ifdef FUTURE
+  if ( data_[ 0 ] == 0xc3 && data_[ 1 ] == 0x81 )
+  {
+#ifdef LOGDATA
+    std::cout << "ProxyServer: Sending server list" << std::endl;
+#endif
+
+    std::string fromIp = AddrInfo::SockAddrToAddress( &client_ );
+    bool fromLocalNetwork = m_Network.OnLocalNetwork( fromIp );
+
+    // If we receive a packet from a remote server then turn ProxyMode ON.
+    if ( !fromLocalNetwork )
+    {
+      m_ProxyMode = true;
+    }
+
+    if ( !m_ProxyMode )
+    {
+      return;
+    }
+
+    m_ServerList.ForEachServer( [ this, &client_, fromLocalNetwork ] ( const ServerEntry &entry_ )
+    {
+      // If it is from the local network then forward only remote servers to sender on local network.
+      if ( !entry_.m_LocalServer )
+      {
+        m_PacketServer.send( client_, entry_.m_frame );
+      }
+
+      const bool continueOn = true;
+      return continueOn;
+    } );
+  }
+  else
+  {
+    std::cout << "ForwardServer: Unknown frame type, ignoring" << std::endl;
+  }
+#endif
+}
+
+
+bool ProxyServer::valid_packet( const std::vector< uint8_t > &data_ )
+{
+  if ( data_.size( ) < 4 )
+  {
+    return false;
+  }
+
+  uint16_t len = data_[ 3 ];
+  len = ( len << 8 ) | data_[ 2 ];
+
+  if ( data_.size( ) - 4 != len )
+  {
+    return false;
+  }
+
+  return true;
+}
---
+++ src/proxy_server.h
@@ -0,0 +1,49 @@
+#pragma once
+
+//
+// The ProxyServer watches for game packets coming from the remote server or
+// the local PS3s and passes the packets through so that WarHawkReborn can
+// act as an intermediary or "proxy" server.
+// This requires the port forwarding to forward to the WarHawkReborn server
+// instead of the local PS3 running WarHawk.
+//
+
+#include <functional>
+#include <iostream>
+
+#include "message_handler.h"
+#include "packet_server.h"
+#include "server_list.h"
+#include "udp_server.h"
+
+class Network;
+
+
+class ProxyServer : public MessageHandler
+{
+  public:
+
+    ProxyServer( ServerList &, PacketServer &, Network & );
+    ~ProxyServer( );
+ 
+    void OnReceivePacket( sockaddr_storage client, std::vector< uint8_t > data ) override;
+
+  protected:
+
+  private:
+
+    //
+    // Methods
+    //
+
+    bool valid_packet( const std::vector< uint8_t > &data_ );
+
+    //
+    // Data
+    //
+
+    ServerList   &m_ServerList;
+    PacketServer &m_PacketServer;
+    Network      &m_Network;
+    bool          m_ProxyMode = false;
+};
